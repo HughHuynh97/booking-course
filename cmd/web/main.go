89e499fe270439af16087f0cee35b549134f5716
hugh.huynh <trihuynh030697@gmail.com>
2024-09-01 21:43:56 +0700

    # Setting up session package

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,14 +4,27 @@ import (
 	"github.com/HughHuynh97/booking-course/pkg/config"
 	"github.com/HughHuynh97/booking-course/pkg/handlers"
 	"github.com/HughHuynh97/booking-course/pkg/render"
+	"github.com/alexedwards/scs/v2"
 	"log"
 	"net/http"
+	"time"
 )
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 func main() {
-	var app config.AppConfig
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/HughHuynh97/booking-course
 go 1.15
 
 require (
+	github.com/alexedwards/scs/v2 v2.8.0 // indirect
 	github.com/go-chi/chi v4.1.2+incompatible
 	github.com/justinas/nosurf v1.1.1 // indirect
 )
